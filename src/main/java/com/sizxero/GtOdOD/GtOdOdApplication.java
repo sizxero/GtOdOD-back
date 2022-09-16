aeb94a5229eb4342199c87683fe9e2d960370325
박유경 <pyk0514@kumoh.ac.kr>
2022-09-16 11:36:59 +0900

    Initial commit

---
+++ src/main/java/com/sizxero/GtOdOD/GtOdOdApplication.java
@@ -0,0 +1,13 @@
+package com.sizxero.GtOdOD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtOdOdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtOdOdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sizxero/GtOdOD/GtOdOdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sizxero.GtOdOD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtOdOdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
